1359e29cc6e5b4ffe6bef86ae43d820d62210892
Eimantas Vaiciunas <eimantas@walkingsmarts.com>
2016-10-08 09:18:04 +0300

    Initialise the project

---
+++ tvarkau/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  tvarkau
+//
+//  Created by Eimantas Vaiciunas on 08/10/2016.
+//  Copyright © 2016 Code For Vilnius. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
